9cdcda79f97f95db531f64547ce34cb9c089abbd
seanwinn <sean.winn@cloudscaling.com>
2014-04-15 16:11:22 -0700

    node updates

--- cookbooks/hostsfile/recipes/default.rb
+++ cookbooks/hostsfile/recipes/default.rb
@@ -1,20 +1,30 @@
-hostsfile_entry '10.0.7.2' do
+hostsfile_entry '10.0.0.6' do
   hostname  'cm'
   unique    true
 end
 
-hostsfile_entry '10.0.0.6' do
+hostsfile_entry '10.0.2.30' do
   hostname  'node-1'
   unique    true
 end
 
-hostsfile_entry '10.0.3.18' do
+hostsfile_entry '10.0.7.2' do
   hostname  'node-2'
   unique    true
 end
 
-hostsfile_entry '10.0.2.18' do
+hostsfile_entry '10.0.16.6' do
   hostname  'node-3'
+  unique    true
+end
+
+hostsfile_entry '10.0.0.18' do
+  hostname  'node-4'
+  unique    true
+end
+
+hostsfile_entry '10.0.3.34' do
+  hostname  'node-5'
   unique    true
 end
 
