f88f6d4fe5fe4c3373326f7fbc37f084f7d9d7c9
root <root@node-3.(none)>
2014-04-15 00:43:58 +0000

    hosts updates

--- cookbooks/hostsfile/recipes/default.rb
+++ cookbooks/hostsfile/recipes/default.rb
@@ -1,5 +1,20 @@
-hostsfile_entry "#{node['cloudera']['server_ip']}" do
+hostsfile_entry '10.0.7.2' do
   hostname  'cm'
+  unique    true
+end
+
+hostsfile_entry '10.0.0.6' do
+  hostname  'node-1'
+  unique    true
+end
+
+hostsfile_entry '10.0.3.18' do
+  hostname  'node-2'
+  unique    true
+end
+
+hostsfile_entry '10.0.2.18' do
+  hostname  'node-3'
   unique    true
 end
 
